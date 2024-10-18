c7fcef3e18692e4379c9ce3432b317f9ad2a4823
ProjectRavel <rafaelcodeid@gmail.com>
2024-10-18 23:59:56 +0700

    add eagle load

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Support\Arr;
@@ -19,7 +20,10 @@ Route::get('/about', function () {
 });
 
 Route::get('/posts', function () {
-    return view('posts', ['title' => 'Blog', 'posts' => Post::all()]);
+
+    $posts = Post::with(['author', 'category'])->paginate(10);
+
+    return view('posts', ['title' => 'Blog', 'posts' => $posts]);
 });
 
 Route::get('/posts/{post:slug}', function (Post $post) {
@@ -28,6 +32,10 @@ Route::get('/posts/{post:slug}', function (Post $post) {
 
 Route::get('/authors/{user:username}', function (User $user) {
     return view('posts', ['title' => count($user->posts) . ' Article by ' . $user->name, 'posts' => $user->posts]);
+});
+
+Route::get('/category/{category:slug}', function (Category $category) {
+    return view('posts', ['title' => 'Category: ' . $category->name . ' ', 'posts' => $category->posts]);
 });
 
 Route::get('/contact', function () {
