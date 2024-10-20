546feccc824224b722dbd5bb319ebdebdd2ee0d6
ProjectRavel <rafaelcodeid@gmail.com>
2024-10-20 23:48:05 +0700

    Fix Height Posts UI

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,20 +7,24 @@
             <div class="grid gap-8 lg:grid-cols-3">
                 @foreach ($posts as $post)
                     <article
-                        class="hover:scale-110 transition-all p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-                        <div class="flex justify-between items-center mb-5 text-gray-500">
-                            <a href="/category/{{ $post->category->slug }}"
-                                class="bg-{{ $post->category->color }}-100 text-{{ $post->category->color }}-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800 hover:underline">
-                                {{ $post->category->name }}
-                            </a>
-                            <span class="text-sm"> {{ $post->created_at->diffForHumans() }} </span>
-                        </div>
+                        class="flex flex-col justify-between hover:scale-110 transition-all p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 h-full">
+                        <div>
+                            <div class="flex justify-between items-center mb-5 text-gray-500">
+                                <a href="/category/{{ $post->category->slug }}"
+                                    class="bg-{{ $post->category->color }}-100 text-{{ $post->category->color }}-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800 hover:underline">
+                                    {{ $post->category->name }}
+                                </a>
+                                <span class="text-sm"> {{ $post->created_at->diffForHumans() }} </span>
+                            </div>
                             <h2
                                 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white hover:underline">
-                                <a href="/posts/{{$post->slug}}"> {{ $post->title }} </a></h2>
-                        <p class="mb-5 font-light text-gray-500 dark:text-gray-400">{{ Str::limit($post->body, 100) }}
-                        </p>
-                        <div class="flex justify-between items-center">
+                                <a href="/posts/{{$post->slug}}"> {{ $post->title }} </a>
+                            </h2>
+                            <p class="mb-5 font-light text-gray-500 dark:text-gray-400">
+                                {{ Str::limit($post->body, 100) }}
+                            </p>
+                        </div>
+                        <div class="flex justify-between items-center mt-auto">
                             <div class="flex items-center space-x-3">
                                 <img class="w-6 h-6 rounded-full"
                                     src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png"
